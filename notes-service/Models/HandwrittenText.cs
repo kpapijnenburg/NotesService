5e541bd603549a38cf7ccb069fa16dc3e5a3da7f
= <=>
2020-03-19 10:04:12 +0100

    Added basic classes

---
+++ notes-service/Models/HandwrittenText.cs
@@ -0,0 +1,18 @@
+namespace notes_service.Models
+{
+    public enum State
+    {
+        Finished, Pending, Error
+    }
+    public class HandwrittenText
+    {
+        public string Image { get; set; }
+        public State state;
+
+        public HandwrittenText(string image, State state)
+        {
+            Image = image;
+            this.state = state;
+        }
+    }
+}
